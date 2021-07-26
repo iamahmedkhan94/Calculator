11c549682579d4fb4c53b24b102f71c86e816cf4
iamahmedkhan94 <iamahmedkhan94@gmail.com>
2021-07-26 22:30:33 +0500

    Fourth Commit

--- src/service/service.js
+++ src/service/service.js
@@ -1,4 +1,12 @@
+/**
+ * Service to use different functions
+ */
 class Service {
+  /**
+   *
+   * @param {string} result
+   * Calculates the result
+   */
   static calculate = (result) => {
     let checkResult = result;
     if (result.includes("--")) {
@@ -13,10 +21,17 @@ class Service {
       return "error";
     }
   };
+  /**
+   * Reset Function to reset value
+   */
   static reset = () => {
     return "";
   };
-
+  /**
+   *
+   * @param {string} result
+   * Returns the value with deleting the latest value
+   */
   static backspace = (result) => {
     return result.slice(0, -1);
   };
